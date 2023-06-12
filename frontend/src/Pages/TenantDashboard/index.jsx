82b1c448c4976a4ab0a57b9353fd40849b8f8945
mustafah1 <89341847+mustafah1@users.noreply.github.com>
2023-06-12 12:55:31 +0100

    navigation between pages created

---
+++ frontend/src/Pages/TenantDashboard/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TenantDashboard = () => {
+  return <div>TenantDashboard</div>;
+};
+
+export default TenantDashboard;
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,2 +1,6 @@
-export { default as Home } from './Home'
-export { default as IfcViewer } from "./IfcViewer"; 
+export { default as Home } from "./Home";
+export { default as IfcViewer } from "./IfcViewer";
+export { default as LandlordDashboard } from "./LandlordDashboard";
+export { default as TenantDashboard } from "./TenantDashboard";
+export { default as Maintainance } from "./Maintainance";
+export { default as Property } from "./Property";
