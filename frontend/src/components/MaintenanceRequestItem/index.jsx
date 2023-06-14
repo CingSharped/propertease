fb1d65767f73ffec3e28ecc86cf45edff1fb3e77
nngw <thanushiyan@hotmail.co.uk>
2023-06-14 11:52:53 +0100

    add tests for maintenence list, style complete and delete buttons, add function placeholder to delete/complete task

--- frontend/src/components/MaintenanceRequestItem/index.jsx
+++ frontend/src/components/MaintenanceRequestItem/index.jsx
@@ -1,44 +1,65 @@
 import "./style.css"
 
-function MaintenanceRequestItem ({request}) {
+function MaintenanceRequestItem ({request, deleteRequest, completeRequest}) {
   return (
     request.completed 
-    ? <li className="maintenance-request-item-completed" role="completed task">
+    ? <li className="maintenance-request-item-completed" role="reqest-item">
       <div className="image-container">IMAGE PLACEHOLDER</div>
       <table className="details-container">
-        <tr>
-          <th>Issue</th>
-          <td>{request.issue}</td>
-        </tr>
-        <tr>
-          <th>Location</th>
-          <td>{request.location}</td>
-        </tr>
-        <tr>
-          <th>Description</th>
-          <td>{request.description}</td>
-        </tr>
+        <tbody>
+          <tr>
+            <th>Issue</th>
+            <td>{request.issue}</td>
+          </tr>
+          <tr>
+            <th>Location</th>
+            <td>{request.location}</td>
+          </tr>
+          <tr>
+            <th>Description</th>
+            <td>{request.description}</td>
+          </tr>
+        </tbody>
       </table>
       <div className="request-editor">
-        hello
+        <button className="trash-btn" onClick={() => deleteRequest(request)}>
+        <span className="material-symbols-outlined">delete</span>
+        </button>
       </div>
     </li> 
-    : <li className="maintenance-request-item" role="open task">
+
+    : <li className="maintenance-request-item" role="reqest-item">
       <div className="image-container">IMAGE PLACEHOLDER</div>
-      <table>
-        <tr>
-          <th>Issue</th>
-          <td>{request.issue}</td>
-        </tr>
-        <tr>
-          <th>Location</th>
-          <td>{request.location}</td>
-        </tr>
-        <tr>
-          <th>Description</th>
-          <td>{request.description}</td>
-        </tr>
+      <table className="details-container">
+        <tbody>
+          <tr>
+            <th>Issue</th>
+            <td>{request.issue}</td>
+          </tr>
+          <tr>
+            <th>Location</th>
+            <td>{request.location}</td>
+          </tr>
+          <tr>
+            <th>Description</th>
+            <td>{request.description}</td>
+          </tr>
+        </tbody>
       </table>
+      <div className="request-editor">
+        <button 
+          className="complete-btn" 
+          role="complete-btn"
+          onClick={() => completeRequest(request)}>
+        <span className="material-symbols-outlined">done_outline</span>
+        </button>
+        <button 
+          className="trash-btn" 
+          role="delete-btn"
+          onClick={() => deleteRequest(request)}>
+        <span className="material-symbols-outlined">delete</span>
+        </button>
+      </div>
     </li>
   )
 }
