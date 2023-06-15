b5d20687a6659776ee4a5eb3e00a43af3fb1a933
jlp <mattlukin@gmail.com>
2023-06-15 20:38:59 +0100

    added loading animation for IFC models

--- frontend/src/Pages/IfcViewer/index.jsx
+++ frontend/src/Pages/IfcViewer/index.jsx
@@ -2,7 +2,6 @@ import React, { useRef, useEffect, useState } from "react";
 import { IfcViewerAPI } from "web-ifc-viewer";
 import * as THREE from "three";
 import PropertiesMenu from "../../components/PropertiesMenu";
-//import ToolbarBottom from "../../components/ToolbarBottom";
 import "./IfcViewer.css";
 
 const IfcViewer = ({ ifcProject }) => {
@@ -11,11 +10,14 @@ const IfcViewer = ({ ifcProject }) => {
   const [isPropertyMenuVisible, setPropertyMenuVisible] = useState(false);
   const [buildingId, setBuildingId] = useState(0); //store the expressId of a building
   //const [isTreeMenuVisible, setTreeMenuVisible] = useState(true);
+  const [loadingIfc, setLoadingIfc] = useState(true);
 
   useEffect(() => {
-    const ifcUrl = "../ifc-models/TESTED_Simple_project_01.ifc";
+    //const ifcUrl = "../ifc-models/TESTED_Simple_project_01.ifc";
+    
+    const ifcUrl = "../ifc-models/rac_basic_sample_project-IFC4-2.ifc"
 
-    //const ifcUrl = "../ifc-models/rac_basic_sample_project.ifc"
+    //const ifcUrl = "../ifc-models/rac_advanced_sample_project-IFC4.ifc";
 
     const handleDoubleClick = async () => {
       const result = await viewer.IFC.selector.pickIfcItem(true);
@@ -26,7 +28,6 @@ const IfcViewer = ({ ifcProject }) => {
 
       const props = await viewer.IFC.getProperties(modelID, id, true, false);
 
-      //not working consistently first time an ifc element is clicked
       console.log(props);
       setSelectedProperties(props);
       if (!isPropertyMenuVisible) {
@@ -45,23 +46,23 @@ const IfcViewer = ({ ifcProject }) => {
     };
 
     const loadIfc = async (url) => {
-      console.log("loading ifc..."); //turn into a state variable and update
-      await viewer.IFC.setWasmPath("../../../");
+ 
+      //await viewer.IFC.setWasmPath("../../../");
       const model = await viewer.IFC.loadIfcUrl(url);
 
       // Setup camera controls, i.e. change initial view
       const controls = viewer.context.ifcCamera.cameraControls;
-      controls.setPosition(7.6, 4.3, 24.8, false);
-      controls.setTarget(-7.1, -0.3, 2.5, false);
+
+      controls.setPosition(-45.6, 6.3, 25.8, false); // X - red / Y - green (up) / Z - blue
+      //controls.setTarget(-7.1, 10, 2.5, false);
 
       //renders shadow for model
       await viewer.shadowDropper.renderShadow(model.modelID);
 
-      //post processing below causing the properties to not load on first click?
+      //post processing below causing the properties to not load on first click
       //  viewer.context.renderer.postProduction.active = true;
 
-      console.log("loaded ifc");
-
+      //for tree menu
       ifcProject = await viewer.IFC.getSpatialStructure(model.modelID);
       //console.log(ifcProject.expressID);
 
@@ -72,16 +73,18 @@ const IfcViewer = ({ ifcProject }) => {
 
       //floorplans
 
-      await viewer.plans.computeAllPlanViews(model.modelID);
+      // await viewer.plans.computeAllPlanViews(model.modelID);
 
-      const allPlans = viewer.plans.getAll(model.modelID);
+      // const allPlans = viewer.plans.getAll(model.modelID);
 
-      for (const plan of allPlans) {
-        const currentPlan = viewer.plans.planLists[model.modelID][plan];
-        console.log(currentPlan);
-    
-      }
+      // for (const plan of allPlans) {
+      //   const currentPlan = viewer.plans.planLists[model.modelID][plan];
+      //   console.log(currentPlan);
 
+      // }
+      console.log("loaded ifc");
+
+      setLoadingIfc(false);
     };
 
     const viewer = new IfcViewerAPI({
@@ -105,7 +108,7 @@ const IfcViewer = ({ ifcProject }) => {
       window.removeEventListener("keydown", handleKeyDown);
       viewer.dispose();
     };
-  }, [buildingId]);
+  }, [buildingId]); //buildingId inside array
 
   const togglePropertyMenu = () => {
     setPropertyMenuVisible(!isPropertyMenuVisible);
@@ -116,7 +119,14 @@ const IfcViewer = ({ ifcProject }) => {
       <div className="button-wrapper">
         <button onClick={togglePropertyMenu}>Toggle Property Menu</button>
       </div>
-      <div>{buildingId}</div>
+      {loadingIfc && (
+        <div id="loader-container">
+          <svg viewBox="25 25 50 50">
+            <circle r="20" cy="50" cx="50"></circle>
+          </svg>
+          <p>Loading...</p>
+        </div>
+      )}
       <div id="viewer-container" ref={containerRef} />
       {isPropertyMenuVisible && (
         <div className="ifc-property-menu bottom-right" id="ifc-property-menu">
