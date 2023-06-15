20866c240a05bee13d4d64d50f23d95da7148501
Francesca Adu <58713149+FrancescaAdu@users.noreply.github.com>
2023-06-15 23:10:35 +0100

    scrollable dashboard modal

--- frontend/src/Pages/Dashboards/index.jsx
+++ frontend/src/Pages/Dashboards/index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './Dashboards.css';
 import { LargeModal, DashboardModal } from '../../components';
 
-function Dashboards() {
+const Dashboards = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const navigateTo = (url) => {
@@ -16,13 +16,13 @@ function Dashboards() {
     <div>
       <div id="main-container">
         <div id="box1" className="fade-in" onClick={() => navigateTo('./ifc')}>
-          Total Number of Tenants:
+          Total Number of Tenants: 16
         </div>
         <div id="box2" className="fade-in" onClick={() => navigateTo('')}>
-          Total Number of Properties:
+          Total Number of Properties: 3
         </div>
         <div id="box3" className="fade-in" onClick={() => navigateTo('')}>
-          Amount of Outstanding Maintenance Orders:
+          Amount of Outstanding Maintenance Orders: 3
         </div>
         <div id="box4" className="fade-in" onClick={() => navigateTo('')}>
           Bar Graph
@@ -32,22 +32,22 @@ function Dashboards() {
           className="fade-in"
           onClick={() => setIsOpen(true)}>
           Property Listing 1<br />
-          Property Location:<br />
-          Current Tenant:
+          Property Location: Tottenham<br />
+          Current Tenant: Harley Quin
           {isOpen && <LargeModal children={<DashboardModal />} setIsOpen={setIsOpen} />}
         </div>
         <div
           id="box6"
           className="fade-in"onClick={() => setIsOpen(true)}>
           Property Listing 2<br />
-          Property Location:<br />
-          Current Tenant:
+          Property Location: Shoreditch<br />
+          Current Tenant: Michael Scott 
           {isOpen && <LargeModal children={<DashboardModal />} setIsOpen={setIsOpen} />}
         </div>
         <div id="box7" className="fade-in"onClick={() => setIsOpen(true)}>
           Property Listing 3<br />
-          Property Location:<br />
-          Current Tenant:
+          Property Location: Notting Hill<br />
+          Current Tenant: Beyonce Knowles
           {isOpen && <LargeModal children={<DashboardModal />} setIsOpen={setIsOpen} />}
         </div>
       </div>
--- frontend/src/components/LargeModal/index.jsx
+++ frontend/src/components/LargeModal/index.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { RiCloseLine } from "react-icons/ri"
+import React from 'react';
+import { RiCloseLine } from "react-icons/ri";
 
 import styles from "./LargeModal.module.css";
 
@@ -7,18 +7,18 @@ const LargeModal = ({ children, setIsOpen }) => {
   return (
     <div className='maintenance-request-form'>
       <div className={styles.modal_darkBG} onClick={() => setIsOpen(false)} />
-        <div className={styles.modal_centered}>
-          <div className={styles.modal_modal}>
-            <button className={styles.modal_closeBtn} onClick={() => setIsOpen(false)}>
-              <RiCloseLine style={{ marginBottom: "-3px" }} />
-            </button>
-            <div className={styles.modal_modalContent}>
-              {children}
-            </div>
+      <div className={styles.modal_centered}>
+        <div className={styles.modal_modal}>
+          <button className={styles.modal_closeBtn} onClick={() => setIsOpen(false)}>
+            <RiCloseLine style={{ marginBottom: "-3px" }} />
+          </button>
+          <div className={`${styles.modal_modalContent} ${styles.scrollableContent}`}>
+            {children}
           </div>
+        </div>
       </div>
     </div>
   );
 }
 
-export default LargeModal
+export default LargeModal;
