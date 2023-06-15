5a78affd8094403d2cb6eb2fd472f4f50e00ddba
nngw <thanushiyan@hotmail.co.uk>
2023-06-15 16:12:53 +0100

    function to post new requests

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,8 +7,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+      <Router>
+        <App />
+      </Router>
   </React.StrictMode>,
 )
