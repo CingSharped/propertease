484c1a37539a9bbed447bc2d239c8adf6300dde4
christophersharpe <chrismsharpe123@gmail.com>
2023-06-19 11:19:10 +0100

    started transactions endpoint

---
+++ backend/application/controllers/transactions.py
@@ -0,0 +1,6 @@
+from application.models.Transaction import Transaction
+
+def create_transaction(property_id, property_owner_id, month, money, transaction_type, workorder_id):
+  # check type options
+  # create transaction class with data
+  # run create_transaction and return it
