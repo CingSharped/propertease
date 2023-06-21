b68b1ee47d6d0827821e5289e5e99ec5ad0c5189
christophersharpe <chrismsharpe123@gmail.com>
2023-06-21 15:17:40 +0100

    changed pages import in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+// import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import * as Pages from "./Pages";
+import * as Pages from "./pages";
 import { NavBar } from './components';
 
 function App() {
