f9003fa01e484db68a7657601ba91af51b5168bd
christophersharpe <chrismsharpe123@gmail.com>
2023-06-21 14:13:59 +0100

    PATCH /workers endpoint can now be used to update workorders

--- backend/application/controllers/workorders.py
+++ backend/application/controllers/workorders.py
@@ -1,8 +1,8 @@
 from application import db
 from application.models.Workorder import Workorder
 
-def create_workorder(title, description, work_type, location_id, cost, status, priority, property_id, property_owner_id, created_on, created_by, completed):
-  new_workorder = Workorder(title, description, work_type, location_id, cost, status, priority, property_id, property_owner_id, created_on, created_by, completed)
+def create_workorder(_id, title, description, work_type, location_id, cost, status, priority, property_id, property_owner_id, created_on, created_by, completed):
+  new_workorder = Workorder(_id, title, description, work_type, location_id, cost, status, priority, property_id, property_owner_id, created_on, created_by, completed)
   return new_workorder.create_workorder()
 
 def get_all_workorders():
@@ -13,3 +13,9 @@ def get_workorders_by_property(_id):
 
 def get_workorders_by_user(_id):
   return Workorder.get_workorders_by_user(_id)
+
+def update_workorder( _id, title, description, work_type, location_id, cost, status, priority, property_id, property_owner_id, created_on, created_by, completed):
+  print(f'in controllers: {_id}')
+  workorder = Workorder( _id, title, description, work_type, location_id, cost, status, priority, property_id, property_owner_id, created_on, created_by, completed)
+  print(f'New workorder: {workorder._id}')
+  return workorder.update_workorder()
