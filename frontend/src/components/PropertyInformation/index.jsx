3cd4c2e7299d86a4afae4488009b92077962abcc
christophersharpe <chrismsharpe123@gmail.com>
2023-06-23 00:44:25 +0100

    landlord dashboard and property information pages/component display property data properly

--- frontend/src/components/PropertyInformation/index.jsx
+++ frontend/src/components/PropertyInformation/index.jsx
@@ -1,74 +1,36 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import './dashboardmodal.css';
-import BarChart from '../BarChart';
+import "./dashboardmodal.css";
+import BarChart from "../BarChart";
 import MaintenanceRequestList from "../MaintenanceRequestList";
 
-
-const PropertyInformation = ({property, setIsOpen}) => {
+const PropertyInformation = ({ property, setIsOpen }) => {
   const [chartData, setChartData] = useState();
   // const user = localStorage.getItem('user')
- 
-    const [data, setData] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
 
-    async function fetchRequests() {
-      try {
-        const res = await fetch(
-          `https://propertease-api.onrender.com/workorders`
-        );
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-        const json = await res.json();
+  async function fetchRequests() {
+    try {
+      const res = await fetch(
+        `https://propertease-api.onrender.com/workorders/properties/648b64ca2431694eaff1d1dc`
+      );
 
-        setData(json);
+      const json = await res.json();
 
-        data.length === 0 ? setIsLoading(false) : "";
-      } catch (error) {
-        console.log("error loading data");
-      }
+      setData(json);
+
+      data.length === 0 ? setIsLoading(false) : "";
+    } catch (error) {
+      console.log("error loading data");
     }
+  }
 
-    useEffect(() => {
-      fetchRequests();
-    }, []);
-  // const transactions = [
-  //   {
-  //     _id: "oiew493hun3j",
-  //     property_id: "i43bh43b3",
-  //     property_owner_id: "ij43ublik3jb",
-  //     month: "January",
-  //     money: "1000",
-  //     transaction_type: "Income",
-  //     workorder_id: "3ikjbol435iuhb",
-  //   },
-  //   {
-  //     _id: "oiew493hun3j",
-  //     property_id: "i43bh43b3",
-  //     property_owner_id: "ij43ublik3jb",
-  //     month: "Feburary",
-  //     money: "2000",
-  //     transaction_type: "Income",
-  //     workorder_id: "3ikjbol435iuhb",
-  //   },
-  //   {
-  //     _id: "oiew493hun3j",
-  //     property_id: "i43bh43b3",
-  //     property_owner_id: "ij43ublik3jb",
-  //     month: "January",
-  //     money: "300",
-  //     transaction_type: "Expense",
-  //     workorder_id: "3ikjbol435iuhb",
-  //   },
-  //   {
-  //     _id: "oiew493hun3j",
-  //     property_id: "i43bh43b3",
-  //     property_owner_id: "ij43ublik3jb",
-  //     month: "Feburary",
-  //     money: "300",
-  //     transaction_type: "Expense",
-  //     workorder_id: "3ikjbol435iuhb",
-  //   },
-  // ];
+  useEffect(() => {
+    fetchRequests();
+  }, []);
+
 
   async function createProfitData() {
         const response = await axios.get(
@@ -84,46 +46,71 @@ const PropertyInformation = ({property, setIsOpen}) => {
         }
       });
       console.log(months);
-
-      const profits = [];
+      const grossProfit = [];
+      const profits = []
+      const expenses = []
       months.map((month) => {
-        profits.push({
+        grossProfit.push({
           month: month,
           profit: 0,
         });
+        profits.push({
+          month: month,
+          income: 0
+        })
+        expenses.push({
+          month: month,
+          expense: 0
+        })
       });
       transactions.map((transaction) => {
         if (transaction.transaction_type == "Income") {
-          profits.forEach((profitData) => {
+          grossProfit.forEach((profitData) => {
             if (profitData.month == transaction.month) {
               profitData.profit =
                 parseInt(profitData.profit) + parseInt(transaction.money);
             }
           });
+          profits.forEach(profitData => {
+            if (profitData.month == transaction.month) {
+              profitData.income = parseInt(transaction.money)
+            }
+          })
         }
         if (transaction.transaction_type == "Expense") {
-          profits.forEach((profitData) => {
+          grossProfit.forEach((profitData) => {
             if (profitData.month == transaction.month) {
               profitData.profit =
                 parseInt(profitData.profit) - parseInt(transaction.money);
             }
           });
+          expenses.forEach(expenseData => {
+            if (expenseData.month == transaction.month) {
+              expenseData.expense = parseInt(transaction.money)
+            }
+          })
         }
+        // grossProfit.map((data) => data.month)
         setChartData({
-          labels: profits.map((data) => data.month),
+          labels: ['January', 'February', 'March', 'April'],
           datasets: [
             {
-              label: "Users Gained",
-              data: profits.map((data) => data.profit),
+              label: "Profits",
+              data: profits.map((data) => data.income),
+            },
+            {
+              label: "Expenses",
+              data: expenses.map((data) => data.expense),
+            },
+            {
+              label: "Gross Profit",
+              data: grossProfit.map((data) => data.profit),
             },
           ],
         });
-
-
       });
       console.log(profits);
       
-
     }
   }
 
@@ -131,36 +118,39 @@ const PropertyInformation = ({property, setIsOpen}) => {
     createProfitData();
   }, []);
 
+  return (
+    <div>
+      <div id="main-container">
+        <h1>{property.title}</h1>
+        <div
+          id="boxno1"
+          className="fade-in"
+          onClick={() => this.navigateTo("./ifc")}
+        >
+          {property.name}
+        </div>
+        <div id="boxno2" className="fade-in">
+          {chartData ? <BarChart chartData={chartData} /> : "Loading..."}
+        </div>
 
-
-    return (
-   
-      <div>
-        <div id="main-container">
-          <h1>{property.title}</h1>
-          <div id="boxno1" className="fade-in" onClick={() => this.navigateTo('./ifc')}>
-            <p>{property.address}</p>
-          </div>
-          <div id="boxno2" className="fade-in">
-            {chartData ? <BarChart chartData={chartData} /> : 'Loading...'}
-          </div>
-          
-          <div id="boxno3" className="fade-in">
-          <p>{property.description}</p>
-          </div>
-          <div id="boxno4" className="fade-in">
-            <p>{property.tenure}</p>
-            <p>{property.energy_rating}</p>
-            <p>{property.bathrooms}</p>
-            <p>{property.council_tax_band}</p>
-          </div>
-          <div id="boxno5" className="fade-in">
-          <MaintenanceRequestList />
-            </div>
+        <div id="boxno3" className="fade-in">
+          Description:
+          <br />
+          <br />
+          {property.description}
+        </div>
+        <div id="boxno4" className="fade-in">
+          <p>Tenure: {property.tenure}</p>
+          <p>Energy Rating: {property.energy_rating}</p>
+          <p>Bathrooms: {property.bathrooms}</p>
+          <p>Council Tax Band: {property.council_tax_band}</p>
+        </div>
+        <div id="boxno5" className="fade-in">
+          <MaintenanceRequestList data={data} isLoading={isLoading} />
         </div>
       </div>
-    
-    );
-  }
+    </div>
+  );
+};
 
 export default PropertyInformation;
--- frontend/src/pages/LandlordDash/index.jsx
+++ frontend/src/pages/LandlordDash/index.jsx
@@ -10,37 +10,77 @@ const Landlord = () => {
   const [profitData, setProfitData] = useState([]);
   const [activeAccordion, setActiveAccordion] = useState(null);
   const [chartData, setChartData] = useState();
-  const [ properties, setProperties ] = useState();
-
-  const [ showProperties, setShowProperties ] = useState(false);
+  // const [ properties, setProperties ] = useState();
 
   const [data, setData] = useState([])
   const [isLoading, setIsLoading] = useState(true)
 
-  const property = {
-    _id: "64944c85abec2e395db04c4e",
-    address: "430 London Rd, Westcliff-on-Sea, Southend-on-Sea",
-    bathrooms: 8,
-    bedrooms: 0,
-    council_tax_band: "F",
-    created_on: "Thu, 15 Jun 2023 20:21:46 GMT",
-    description: "The Palace Theatre, located in Southend on Sea",
-    energy_rating: "D",
-    model_id: "jhbv43kjh34jh",
-    name: "The Palace Theatre",
-    owner_id: "648b1ecf800c2596b4c79aa1",
-    postcode: "SS0 9LA",
-    property_type: "Terraced",
-    rent_date: null,
-    rental_cost: 5000,
-    tenant_id: "648b1ecf800c2596b4c79aa1",
-    tenure: null,
-  };
+  const properties = [
+    {
+      _id: "648b64ca2431694eaff1d1dc",
+      address: "125 Furtherwick Road, Canvey Island",
+      bathrooms: 4,
+      bedrooms: 0,
+      council_tax_band: "E",
+      created_on: "Thu, 15 Jun 2023 20:21:46 GMT",
+      description: "A local gym, located on Canvey Island",
+      energy_rating: "C",
+      model_id: "jhbv43kjh34jh",
+      name: "Anytime Fitness Canvey Island",
+      owner_id: "648b1ecf800c2596b4c79aa1",
+      postcode: "SS8 7AT",
+      property_type: "Terraced",
+      rent_date: null,
+      rental_cost: 1500,
+      tenant_id: "648b1ecf800c2596b4c79aa1",
+      tenure: "Leasehold",
+    },
+    {
+      _id: "64944a60abec2e395db04c4d",
+      address: "The Broadway, London Rd, Southend-on-Sea",
+      bathrooms: 8,
+      bedrooms: 0,
+      council_tax_band: "F",
+      created_on: "Thu, 15 Jun 2023 20:21:46 GMT",
+      description: "A local cinema, located in Southend on Sea",
+      energy_rating: "E",
+      model_id: "jhbv43kjh34jh",
+      name: "Odeon Cinema",
+      owner_id: "648b1ecf800c2596b4c79aa1",
+      postcode: "SS1 1TJ",
+      property_type: "Terraced",
+      rent_date: null,
+      rental_cost: 5000,
+      tenant_id: '648b1ecf800c2596b4c79aa1',
+      tenure: "Leasehold",
+    },
+    {
+      _id: "64944c85abec2e395db04c4e",
+      address: "430 London Rd, Westcliff-on-Sea, Southend-on-Sea",
+      bathrooms: 8,
+      bedrooms: 0,
+      council_tax_band: "F",
+      created_on: "Thu, 15 Jun 2023 20:21:46 GMT",
+      description: "The Palace Theatre, located in Southend on Sea",
+      energy_rating: "D",
+      model_id: "jhbv43kjh34jh",
+      name: "The Palace Theatre",
+      owner_id: "648b1ecf800c2596b4c79aa1",
+      postcode: "SS0 9LA",
+      property_type: "Terraced",
+      rent_date: null,
+      rental_cost: 5000,
+      tenant_id: "648b1ecf800c2596b4c79aa1",
+      tenure: "Leasehold",
+    },
+  ];
 
 
   async function fetchRequests () {
     try {
-      const res = await fetch(`https://propertease-api.onrender.com/workorders`)
+      const res = await fetch(
+        `https://propertease-api.onrender.com/workorders/users/648b1ecf800c2596b4c79aa1`
+      );
   
       const json = await res.json()
       
@@ -61,20 +101,19 @@ const Landlord = () => {
   useEffect(() => {
     fetchRequests()
     createProfitData();
-    getProperties();
-    setShowProperties(true);
+    // getProperties();
   },[])
 
-  async function getProperties() {
-    const response = await axios.get(
-      `https://propertease-api.onrender.com/properties/users/648b1ecf800c2596b4c79aa1`
-    );
-    const properties = response.data
+  // async function getProperties() {
+  //   const response = await axios.get(
+  //     `https://propertease-api.onrender.com/properties/users/648b1ecf800c2596b4c79aa1`
+  //   );
+  //   const properties = response.data
 
-    setProperties(properties)
-    console.log(properties)
+  //   setProperties(properties)
+  //   console.log(properties)
 
-  }
+  // }
 
 
   async function createProfitData() {
@@ -91,36 +130,65 @@ const Landlord = () => {
         }
       });
       console.log(months);
-      const profits = [];
+      const grossProfit = [];
+      const profits = []
+      const expenses = []
       months.map((month) => {
-        profits.push({
+        grossProfit.push({
           month: month,
           profit: 0,
         });
+        profits.push({
+          month: month,
+          income: 0
+        })
+        expenses.push({
+          month: month,
+          expense: 0
+        })
       });
       transactions.map((transaction) => {
         if (transaction.transaction_type == "Income") {
-          profits.forEach((profitData) => {
+          grossProfit.forEach((profitData) => {
             if (profitData.month == transaction.month) {
               profitData.profit =
                 parseInt(profitData.profit) + parseInt(transaction.money);
             }
           });
+          profits.forEach(profitData => {
+            if (profitData.month == transaction.month) {
+              profitData.income = parseInt(transaction.money)
+            }
+          })
         }
         if (transaction.transaction_type == "Expense") {
-          profits.forEach((profitData) => {
+          grossProfit.forEach((profitData) => {
             if (profitData.month == transaction.month) {
               profitData.profit =
                 parseInt(profitData.profit) - parseInt(transaction.money);
             }
           });
+          expenses.forEach(expenseData => {
+            if (expenseData.month == transaction.month) {
+              expenseData.expense = parseInt(transaction.money)
+            }
+          })
         }
+        // grossProfit.map((data) => data.month)
         setChartData({
-          labels: profits.map((data) => data.month),
+          labels: ['January', 'February', 'March', 'April'],
           datasets: [
             {
-              label: "Users Gained",
-              data: profits.map((data) => data.profit),
+              label: "Profits",
+              data: profits.map((data) => data.income),
+            },
+            {
+              label: "Expenses",
+              data: expenses.map((data) => data.expense),
+            },
+            {
+              label: "Gross Profit",
+              data: grossProfit.map((data) => data.profit),
             },
           ],
         });
@@ -157,19 +225,25 @@ const Landlord = () => {
 
         <div id="main-container">
           <div id="box1" className="fade-in">
-            Total Number of Tenants: 16
+            Total Tenants:{" "}
+            {properties.reduce((acc, property) => {
+              if (property.tenant_id != null) {
+                return acc + 1;
+              }
+              return acc;
+            }, 0)}
           </div>
           <div id="box2" className="fade-in">
-            Total Number of Properties: 3
+            Total Properties: {properties.length}
           </div>
           <div id="box3" className="fade-in">
-            Amount of Outstanding Maintenance Orders: 3
+            Outstanding Maintenance Orders: {data.length}
           </div>
 
           <h4>Overall profit per month</h4>
 
           <div id="box4" className="fade-in">
-          {chartData ? <BarChart chartData={chartData} /> : 'Loading...'}
+            {chartData ? <BarChart chartData={chartData} /> : "Loading..."}
           </div>
 
           <button
@@ -178,47 +252,62 @@ const Landlord = () => {
           >
             All Properties
           </button>
-            <div className={`panel1 ${activeAccordion === 1 ? "show" : ""}`}>
-
+          <div className={`panel1 ${activeAccordion === 1 ? "show" : ""}`}>
             <div id="box5" className="fade-in" onClick={() => setIsOpen(true)}>
-              Property Listing 1<br />
-              <p>{ property.address}</p>
-              
+              Current Tenant: Romeo Saint Albin
               <br />
-              Current Tenant: Harley Quin
+              Address: {properties[0].address}
+              <br />
+              Postcode: {properties[0].postcode}
               {isOpen && (
                 <LargeModal
-                  children={<PropertyInformation property={ property} setIsOpen={setIsOpen} />}
+                  children={
+                    <PropertyInformation
+                      property={properties[0]}
+                      setIsOpen={setIsOpen}
+                    />
+                  }
                   setIsOpen={setIsOpen}
                 />
               )}
             </div>
             <div id="box6" className="fade-in" onClick={() => setIsOpen(true)}>
-              Property Listing 2<br />
-              <p>{ property.address}</p>
+              Current Tenant: Ree Gilling
               <br />
-              Current Tenant: Michael Scott
+              Address: {properties[1].address}
+              <br />
+              Postcode: {properties[1].postcode}
               {isOpen && (
                 <LargeModal
-                  children={<PropertyInformation property={ property} setIsOpen={setIsOpen} />}
+                  children={
+                    <PropertyInformation
+                      property={properties[1]}
+                      setIsOpen={setIsOpen}
+                    />
+                  }
                   setIsOpen={setIsOpen}
                 />
               )}
             </div>
             <div id="box7" className="fade-in" onClick={() => setIsOpen(true)}>
-              Property Listing 3<br />
-              <p>{ property.address}</p>
+              Current Tenant: Emile Sherrott
               <br />
-              Current Tenant: Beyonce Knowles
+              Address: {properties[2].address}
+              <br />
+              Postcode: {properties[2].postcode}
               {isOpen && (
                 <LargeModal
-                  children={<PropertyInformation property={ property} setIsOpen={setIsOpen}/>}
+                  children={
+                    <PropertyInformation
+                      property={properties[2]}
+                      setIsOpen={setIsOpen}
+                    />
+                  }
                   setIsOpen={setIsOpen}
                 />
               )}
             </div>
-          </div> )}
-         
+          </div>
         </div>
 
         <button
@@ -228,7 +317,7 @@ const Landlord = () => {
           All Work Orders
         </button>
         <div className={`panel2 ${activeAccordion === 2 ? "show" : ""}`}>
-        <MaintenanceRequestList data={data} isLoading={isLoading}/>
+          <MaintenanceRequestList data={data} isLoading={isLoading} />
         </div>
       </div>
       <Footer />
